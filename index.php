c0f27357055e23d2a5f9b0d317bae7cf9ddb0460
“Taiseer <thussein@redhat.com>
2024-05-28 16:12:25 +0000

    changed my php code

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo '<body style="background-color:green">';
-print "Hello, Test GitOps. test" . PHP_VERSION . "\n";
+echo '<body style="background-color:yellow">';
+print "Hello, Test GitOps." . PHP_VERSION . "\n";
 ?>
